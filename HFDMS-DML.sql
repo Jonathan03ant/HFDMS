0fcd7dcef337dfaa15bb72f6f25bd9023bf5b10d
jonathan <jonathan03ant@gmail.com>
2024-04-12 00:21:51 -0400

    ddl and DDL on progress

---
+++ HFDMS-DML.sql
@@ -0,0 +1,23 @@
+-- Inserting Swimming Trainers
+INSERT INTO Trainers (FullName)
+VALUES ('SWM T1'), ('SWM T2'), ('SWM T3'), ('SWM T4');
+
+-- Inserting GroupFitness Trainers
+INSERT INTO Trainers (FullName)
+VALUES ('GF T1'), ('GF T2'), ('GF T3'), ('GF T4');
+
+-- Inserting BodyBuilding Trainers
+INSERT INTO Trainers (FullName)
+VALUES ('BDYB T1'), ('BDYB T2'), ('BDYB T3'), ('BDYB T4'), ('BDYB T5'), ('BDYB T6');
+
+-- Inserting Cardio Trainers
+INSERT INTO Trainers (FullName)
+VALUES ('CAR T1'), ('CAR T2'), ('CAR T3'), ('CAR T4'), ('CAR T5'), ('CAR T6'), ('CAR T7'), ('CAR T8');
+
+-- Inserting MarshalArts Trainers
+INSERT INTO Trainers (FullName)
+VALUES ('MLAT T1'), ('MLAT T2');
+
+-- Inserting Boxing Trainers
+INSERT INTO Trainers (FullName)
+VALUES ('BOX T1'), ('BOX T2');
