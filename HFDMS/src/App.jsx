6aedecf8f0fbb70067c250f0ec07356a1f996f76
jonathan <jonathan03ant@gmail.com>
2024-04-13 04:51:36 -0400

    App.jsx now has The AdminHomePage---Next--Nav--

--- HFDMS/src/App.jsx
+++ HFDMS/src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FrontPage from './Components/FrontPage'; // Adjust the import path as necessary
-import AdminPage from './Components/AdminPage'; // Adjust the import path as necessary
-import TrainerPage from './Components/TrainerPage'; // Adjust the import path as necessary
-import MemberPage from './Components/MemberPage'; // Adjust the import path as necessary
+import FrontPage from './Components/FrontPage'; 
+import AdminLandingPage from './Components/AdminLandingPage'; 
+import TrainerPage from './Components/TrainerPage'; 
+import MemberPage from './Components/MemberPage'; 
+import AdminHomePage from './Components/AdminHomePage';
 
 function App() {
  return (
     <Router>
       <Routes>
         <Route path="/" element={<FrontPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin" element={<AdminLandingPage />} />
+        <Route path="/admin-home" element={<AdminHomePage />} />
+
+
         <Route path="/trainer" element={<TrainerPage />} />
         <Route path="/member" element={<MemberPage />} />
       </Routes>
